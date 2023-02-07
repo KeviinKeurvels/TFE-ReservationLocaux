d6ff920e5d60100d9e9b4b9aa5ac85ab98299147
Kevin Keurvels <kevinkeurvels@hotmail.be>
2023-02-07 22:24:39 +0100

    Changements fonctionnalités importantes
    - Suppression des commentaires explicatifs de Flutter
    - Ajout du code permettant d'afficher des cartes et des textes, tout cela dans une application qui slide vers le bas.
    
    - Tout ceci n'est que pour s'entrainer.

--- ephecreservationapp/lib/main.dart
+++ ephecreservationapp/lib/main.dart
@@ -7,24 +7,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'EPHEC RESERVATION',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Color.fromARGB(255, 236, 143, 20),
+          secondary: Color.fromARGB(255, 236, 143, 20),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'EPHEC RESERVATION'),
     );
   }
 }
@@ -52,64 +45,223 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+      //quand on clique sur le nombre pour rebuild
       _counter++;
     });
   }
 
+  void getNext() {
+    print("appel à la fonction");
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    //va rerun en même temps que setState
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Center(
+            child: Column(
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Counter(counter: _counter),
+                Wrap(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        getNext();
+                      },
+                      child: Text('Test'),
+                    ),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        getNext();
+                      },
+                      icon: Icon(Icons.favorite_border),
+                      label: Text('Like'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        getNext();
+                      },
+                      child: Text('Test'),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          InkWell(
+            child: Card(
+              elevation: 8,
+              shadowColor: Colors.black,
+              margin: EdgeInsets.all(20),
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.white)),
+              child: ListTile(
+                title: Text("Codesinsider.com"),
+              ),
             ),
-          ],
-        ),
+            onTap: () {
+              print("Click event on Container");
+            },
+          ),
+          InkWell(
+            child: Card(
+              elevation: 8,
+              shadowColor: Colors.black,
+              margin: EdgeInsets.all(20),
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.white)),
+              child: ListTile(
+                title: Text("Codesinsider.com"),
+              ),
+            ),
+            onTap: () {
+              print("Click event on Container");
+            },
+          ),
+          InkWell(
+            child: Card(
+              elevation: 8,
+              shadowColor: Colors.black,
+              margin: EdgeInsets.all(20),
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.white)),
+              child: ListTile(
+                title: Text("Codesinsider.com"),
+              ),
+            ),
+            onTap: () {
+              print("Click event on Container");
+            },
+          ),
+          InkWell(
+            child: Card(
+              elevation: 8,
+              shadowColor: Colors.black,
+              margin: EdgeInsets.all(20),
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.white)),
+              child: ListTile(
+                title: Text("Codesinsider.com"),
+              ),
+            ),
+            onTap: () {
+              print("Click event on Container");
+            },
+          ),
+          InkWell(
+            child: Card(
+              elevation: 8,
+              shadowColor: Colors.black,
+              margin: EdgeInsets.all(20),
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.white)),
+              child: ListTile(
+                title: Text("Codesinsider.com"),
+              ),
+            ),
+            onTap: () {
+              print("Click event on Container");
+            },
+          ),
+          InkWell(
+            child: Card(
+              elevation: 8,
+              shadowColor: Colors.black,
+              margin: EdgeInsets.all(20),
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.white)),
+              child: ListTile(
+                title: Text("Codesinsider.com"),
+              ),
+            ),
+            onTap: () {
+              print("Click event on Container");
+            },
+          ),
+          InkWell(
+            child: Card(
+              elevation: 8,
+              shadowColor: Colors.black,
+              margin: EdgeInsets.all(20),
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.white)),
+              child: ListTile(
+                title: Text("Codesinsider.com"),
+              ),
+            ),
+            onTap: () {
+              print("Click event on Container");
+            },
+          ),
+        ]),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+    );
+  }
+}
+
+class Counter extends StatelessWidget {
+  const Counter({
+    super.key,
+    required int counter,
+  }) : _counter = counter;
+
+  final int _counter;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!.copyWith(
+      //je copy le style et je change juste la couleur
+      backgroundColor: Colors.grey,
+      color: Colors.black,
+    );
+    return Card(
+      color: Colors.grey,
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Text(
+          '$_counter',
+          style: style,
+        ),
+      ),
     );
   }
 }
