cd945326b120fcf4c97dea617aae399dd518a800
Kevin Keurvels <kevinkeurvels@hotmail.be>
2023-02-18 18:28:43 +0100

    US 3 - Automatisation
    - Schedule.tsx: quand on clique sur un jour différent, il va refaire une requete pour voir quelles réservations il y a pour ce jour là
    
    - index.js: on sélectionne les informations qui nous intéresse et on converti hourEnd et hourBegin pour que n'apparaissent que les heures et les minutes

--- ephecReservation/client/src/pages/Schedule.tsx
+++ ephecReservation/client/src/pages/Schedule.tsx
@@ -1,48 +1,72 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem } from '@ionic/react';
 import { IonDatetime, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/react';
 import { useState, useEffect } from 'react';
-import axios from "axios";
 
 import './Schedule.css';
 
 import config from "../config.json";
 
 const Schedule: React.FC = () => {
+  //le jour d'aujourd'hui
+  let currentDate = new Date();
+  //l'année actuelle
+  let currentYear = currentDate.getFullYear();
+  
+  //pour avoir les reservations d'un jour
   const [reservations, setReservations] = useState([]);
-  const fetchOneReservation= async()=>{
-    try{
-      fetch(config.API_URL + "/reservations")
-      .then((res) => res.json())
-      .then((res) => {
-      
-      setReservations(res);
-    })}
-    catch(err){
-      console.log(err);
-    }
-  }
-  /*Appelle la fonction getDetailAnnonce au début du composant*/
+
+  const [dateChosen, setDateChosen] = useState(formatDate(currentDate));
+
+      /*
+      *   Récupère les informations d'une réservation pour un jour
+    */
+      const fetchOneReservation = async () => {
+        try {
+          fetch(config.API_URL + "/reservations/byDay?day='"+dateChosen+"'")
+            .then((res) => res.json())
+            .then((res) => {
+              setReservations(res);
+            })
+        }
+        catch (err) {
+          console.log(err);
+        }
+      }
+
+
+  //le useEffect de dateChosen qui fait que quand on change de date, il va re fetch
   useEffect(() => {
-        /*
-   *   Récupère les détails d'une proposition
-   */
 
     fetchOneReservation()
-  }, []);
-  let currentYear= new Date().getFullYear();
+  }, [dateChosen]);
 
-  function getInformationFromADate(date : any){
+
+  function getInformationFromADate(date: any) {
     //si la date est bien du bon format et bon compris dans la range d'année
-    if([currentYear-1, currentYear, currentYear+1,currentYear+2,currentYear+3].includes(new Date(date).getFullYear())){
-      console.log(new Date(date));
+    if ([currentYear - 1, currentYear, currentYear + 1, currentYear + 2, currentYear + 3].includes(new Date(date).getFullYear())) {
+      let goodFormatDate=formatDate(date);
+      setDateChosen(goodFormatDate);
     }
-    else{//si mauvaise date
+    else {//si mauvaise date
       return -1;
     }
-    
+
   }
 
+  //pour avoir la date dans le bon format pour la DB
+  function formatDate(date : any) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
 
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
 
 
   return (
@@ -55,28 +79,29 @@ const Schedule: React.FC = () => {
       <IonContent fullscreen>
         <h1 id="title_schedule">Choisissez un jour</h1>
         <IonItem>
-        <IonDatetime
-        presentation="date"
-        min={String(currentYear-1)}
-        max={String(currentYear+3)}
-        onIonChange={(e)=>getInformationFromADate(e.target.value)}
-      >
-      </IonDatetime>
+          <IonDatetime
+            presentation="date"
+            value={dateChosen}
+            min={String(currentYear - 1)}
+            max={String(currentYear + 3)}
+            onIonChange={(e) =>getInformationFromADate(e.target.value)}
+          >
+          </IonDatetime>
         </IonItem>
 
         <h1 id="title_schedule">Réservations</h1>
 
-        {reservations.map(reservation=>(
-                  <IonCard color="warning" key={reservation["idRe"]}>
-                  <IonCardHeader>
-                    <IonCardTitle>{reservation["idRe"]}</IonCardTitle>
-                    <IonCardSubtitle>{reservation["hourBegin"]} - {reservation["hourEnd"]}</IonCardSubtitle>
-                  </IonCardHeader>
-        
-                  <IonCardContent>
-                  {reservation["title"]}
-                  </IonCardContent>
-                </IonCard>
+        {reservations.map(reservation => (
+          <IonCard color="warning" key={reservation["idRe"]}>
+            <IonCardHeader>
+              <IonCardTitle>Arnaud Dewulf</IonCardTitle>
+              <IonCardSubtitle>{reservation["hourBegin"]} - {reservation["hourEnd"]}</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+              {reservation["title"]}
+            </IonCardContent>
+          </IonCard>
         ))
 
         }
