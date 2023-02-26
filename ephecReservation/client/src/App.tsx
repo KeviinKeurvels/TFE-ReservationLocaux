6c71496149a08e2290eadf5584c42fea987f2e26
Kevin Keurvels <kevinkeurvels@hotmail.be>
2023-02-26 14:09:40 +0100

    Page Schedule par local + Page Déconnexion
    - Ajout des fichiers pour la page de déconnexion (SignOut)
    - Ajout du fait que la page Schedule va charger les réservations en fonction du local cliqué plus tôt
    
    - Problème: Quand on clique sur un local et puis qu'on revient et qu'on clique sur un autre local, l'affichage dans Schedule ne se reload pas sauf si on clique sur une autre date

--- ephecReservation/client/src/App.tsx
+++ ephecReservation/client/src/App.tsx
@@ -10,12 +10,13 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { homeOutline, accessibilityOutline} from 'ionicons/icons';
+import { homeOutline, accessibilityOutline, logOutOutline} from 'ionicons/icons';
 
 /*pages*/
 import Room from './pages/Room/Room';
 import Schedule from './pages/Schedule/Schedule';
 import Home from './pages/Home/Home';
+import SignOut from './pages/SignOut/SignOut';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -46,11 +47,14 @@ const App: React.FC = () => (
           <Route exact path="/room">
             <Room />
           </Route>
-          <Route exact path="/schedule">
+          <Route exact path="/schedule/:idRoom">
             <Schedule />
           </Route>
           <Route exact path="/home">
             <Home />
+          </Route>
+          <Route exact path="/signOut">
+            <SignOut />
           </Route>
           <Route exact path="/">
             <Redirect to="/home" />
@@ -61,14 +65,22 @@ const App: React.FC = () => (
             <IonIcon icon={homeOutline} />
             <IonLabel>Locaux</IonLabel>
           </IonTabButton>
-          <IonTabButton tab="schedule" href="/schedule">
-            <IonIcon icon={accessibilityOutline} />
-            <IonLabel>Perso</IonLabel>
+
+          <IonTabButton tab="signOut" href="/signOut">
+            <IonIcon icon={logOutOutline} />
+            <IonLabel>Déconnexion</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
     </IonReactRouter>
   </IonApp>
+  
 );
 
 export default App;
+
+/*          <IonTabButton tab="home" href="/">
+            <IonIcon icon={accessibilityOutline} />
+            <IonLabel>Perso</IonLabel>
+          </IonTabButton>
+          */
--- ephecReservation/client/src/pages/SignOut/SignOut.tsx
+++ ephecReservation/client/src/pages/SignOut/SignOut.tsx
@@ -1,31 +1,33 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
-import { IonDatetime, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent } from '@ionic/react';
 import { useHistory } from 'react-router-dom';
-import './Home.css';
+import './SignOut.css';
 
 import config from "../../config.json";
 
-const Home: React.FC = () => {
+const SignOut: React.FC = () => {
 
   const history = useHistory();
-  function redirectToRoom() {
-    history.push("/room");
+  function redirectToRoom(){
+    history.push("/");
+  }
+
+
+  function SignOut (){
+    //code pour déconnexion 
   }
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar id="top_bar">
-          <IonTitle>Accueil</IonTitle>
+          <IonTitle>Déconnexion</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonButton className='button_connexion' onClick={() => redirectToRoom()}>Connexion</IonButton>
-
 
       </IonContent>
     </IonPage>
   );
 };
 
-export default Home;
+export default SignOut;
