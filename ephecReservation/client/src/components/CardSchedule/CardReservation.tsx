5e049e0125709018f16316e302750e90f2f83521
Kevin Keurvels <kevinkeurvels@hotmail.be>
2023-03-05 18:31:39 +0100

    Fix BUG: Défilement des cartes
    - Fix du bug qui créait une deuxième scrollbar
    - C'était à cause du deuxième IonContent qui a été remplacé par un div
    - Merci à Tommy Riquet pour son aide

--- ephecReservation/client/src/components/CardSchedule/CardReservation.tsx
+++ ephecReservation/client/src/components/CardSchedule/CardReservation.tsx
@@ -1,4 +1,4 @@
-import { IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonContent, IonCardSubtitle} from '@ionic/react';
+import { IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonItem, IonCardSubtitle } from '@ionic/react';
 
 
 import './CardReservation.css'
@@ -10,23 +10,24 @@ type CardReservationProps = {
 const CardReservation = ({ Reservations }: CardReservationProps) => {
 
   return (
-    <IonContent>
-        {Reservations.length!=0 ? Reservations.map((reservation : any) => (
-          <IonCard color="warning" key={reservation["idRe"]}>
-            <IonCardHeader>
-              <IonCardTitle>Arnaud Dewulf</IonCardTitle>
-              <IonCardSubtitle>{reservation["hourBegin"]} - {reservation["hourEnd"]}</IonCardSubtitle>
-            </IonCardHeader>
+    <div className='content_reservation'>
+      <h1 id="title_schedule">Réservations</h1>
+      {Reservations.length != 0 ? Reservations.map((reservation: any) => (
+        <IonCard color="warning" key={reservation["idRe"]}>
+          <IonCardHeader>
+            <IonCardTitle>Arnaud Dewulf</IonCardTitle>
+            <IonCardSubtitle>{reservation["hourBegin"]} - {reservation["hourEnd"]}</IonCardSubtitle>
+          </IonCardHeader>
 
-            <IonCardContent>
-              {reservation["title"]}
-            </IonCardContent>
-          </IonCard>
-        ))
-          :
-          <h3 className='noReservation'>Aucune réservations pour le moment.</h3>
-        }
-    </IonContent>
+          <IonCardContent>
+            {reservation["title"]}
+          </IonCardContent>
+        </IonCard>
+      ))
+        :
+        <h3 className='noReservation'>Aucune réservations pour le moment.</h3>
+      }
+    </div>
   );
 };
 
