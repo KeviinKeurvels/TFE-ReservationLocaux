d508b0b5edef2918c9e8d0fd464f472359f62464
Kevin Keurvels <kevinkeurvels@hotmail.be>
2023-03-26 14:00:11 +0200

    Test page Schedule
    - Ajout des tests pour les diverses fonctions de la page Schedule.tsx (certaines fonctions ressemblent fortement à celles de CardReservations.js à la différence que les paramètres ne sont pas les mêmes et que la logique varie légèrement.

--- ephecReservation/client/src/pages/Schedule/Schedule.tsx
+++ ephecReservation/client/src/pages/Schedule/Schedule.tsx
@@ -22,7 +22,7 @@ import './Schedule.css';
 import CardSchedule from '../../components/CardSchedule/CardSchedule';
 import ModalLoading from '../../components/ModalLoading/ModalLoading';
 import config from "../../config.json";
-
+import { getInformationFromADate, formatDate, allFieldsChecked } from '../../functions/Schedule/Schedule';
 
 
 
@@ -69,101 +69,6 @@ const Schedule: React.FC = () => {
   }, [dateChosen]);
 
 
-  function getInformationFromADate(date: any) {
-    //si la date est bien du bon format et bon compris dans la range d'année
-    if ([currentYear, currentYear + 1, currentYear + 2].includes(new Date(date).getFullYear())) {
-      let goodFormatDate = formatDate(date);
-      setDateChosen(goodFormatDate);
-    }
-    else {//si mauvaise date
-      return -1;
-    }
-
-  }
-
-
-
-
-  function formatDate(date: any) {
-    //pour avoir la date dans le bon format pour la DB
-    var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
-      year = d.getFullYear();
-
-    if (month.length < 2)
-      month = '0' + month;
-    if (day.length < 2)
-      day = '0' + day;
-
-    return [year, month, day].join('-');
-  }
-
-  function checkIfThereIsAlreadyAReservation(hourBegin: any, hourEnd: any) {
-    //cette fonction va regarder s'il y a déjà une réservation à ce moment-là
-    let alreadyReserved = false;
-    reservations.forEach((reservation) => {
-      // les heures de la réservation existante 
-      //on ajout et enleve 60000 afin de pouvoir réserver à la minute où l'autre a fini (et vice versa)
-      var x = new Date(dateChosen + " " + reservation["hourBegin"]).getTime() + 60000;
-      var y = new Date(dateChosen + " " + reservation["hourEnd"]).getTime() - 60000;
-
-      // les heures de la nouvelle réservation
-      var a = new Date(dateChosen + " " + hourBegin).getTime();
-      var b = new Date(dateChosen + " " + hourEnd).getTime();
-
-      if (Math.min(x, y) <= Math.max(a, b) && Math.max(x, y) >= Math.min(a, b)) {
-        //si il y a déjà une réservation à ce moment-là
-        alreadyReserved = true;
-      }
-    })
-    return alreadyReserved;
-
-  }
-
-  function allFieldsChecked(form: any) {
-    //cette fonction va regarder si tous les champs sont conformes
-    //pour la box qui va afficher les messages lors de la réservation
-    let responseBox = document.getElementById("callbackMessage");
-
-    //variable qui va contenir le message d'erreur
-    let problem = undefined;
-    if (form.day.value < currentYear || form.day.value > currentYear + 2) {
-      problem = "La date sélectionnée n'est pas bonne";
-    }
-    else if(form.day.value === "" ||form.hourBegin.value === "" ||form.hourEnd.value === ""){
-      problem = "Un ou plusieurs champs sont vides";
-    }
-    else if (form.nameReservation.value.length < 2 || form.nameReservation.value.length > 40) {
-      problem = "L'intitulé n'est pas de taille acceptable (entre 2-40 caractères)";
-    }
-    else if (form.hourBegin.value > "18:00" || form.hourBegin.value < "08:00") {
-      problem = "L'heure de début n'est pas comprise dans les heures d'ouverture du local";
-    }
-    else if (form.hourEnd.value > "18:00" || form.hourEnd.value < "08:00") {
-      problem = "L'heure de fin n'est pas comprise dans les heures d'ouverture du local";
-    }
-    else if (form.hourBegin.value > form.hourEnd.value) {
-      problem = "L'heure de fin ne peut pas être avant l'heure de début";
-    }
-    else if (form.hourBegin.value === form.hourEnd.value) {
-      problem = "L'heure de début ne peut pas être égale à l'heure de fin";
-    }
-    //si déjà une réservation à ce moment-là
-    else if (checkIfThereIsAlreadyAReservation(form.hourBegin.value, form.hourEnd.value)) {
-      problem = "Il y a déjà une réservation à ce moment-là.";
-    }
-
-    if (problem !== undefined) {
-      if (responseBox !== undefined && responseBox !== null) {
-        responseBox.innerHTML = "<p id='failed_response'>" + problem + "</p>";
-      }
-      return false;
-    }
-    return true;
-
-  }
-
   function handleSubmit(event: any) {
     //cache le bouton
     let submitButton = document.getElementById("submit_button");
@@ -183,7 +88,7 @@ const Schedule: React.FC = () => {
     }
 
 
-    if (allFieldsChecked(event.target)) {
+    if (allFieldsChecked(event.target, reservations, currentYear)) {
       //si tous les champs respectent bien ce qu'il faut
 
       fetch(config.API_URL + "/reservations", {
@@ -225,7 +130,7 @@ const Schedule: React.FC = () => {
           }
         })
     }
-    else{
+    else {
       if (submitButton !== undefined && submitButton !== null) {
         submitButton.style.display = "block";
       }
@@ -250,7 +155,7 @@ const Schedule: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-      <ModalLoading isLoading={isLoading} />
+        <ModalLoading isLoading={isLoading} />
         <h1 id="title_schedule">Choisissez un jour</h1>
         <IonItem>
           <IonDatetime
@@ -258,19 +163,19 @@ const Schedule: React.FC = () => {
             value={dateChosen}
             min={String(currentYear)}
             max={String(currentYear + 2)}
-            onIonChange={(e) => getInformationFromADate(e.target.value)}
+            onIonChange={(e) => getInformationFromADate(e.target.value, setDateChosen, currentYear)}
           >
           </IonDatetime>
         </IonItem>
         <IonRow>
           <IonCol>
-          <h1 id="title_schedule">Réservations</h1>
+            <h1 id="title_schedule">Réservations</h1>
           </IonCol>
           <IonCol>
-          <IonButton fill="outline" className='reservation_top_button' onClick={()=>fetchAllReservationForOneDay()}><IonIcon icon={refreshOutline} /></IonButton>
+            <IonButton fill="outline" className='reservation_top_button' onClick={() => fetchAllReservationForOneDay()}><IonIcon icon={refreshOutline} /></IonButton>
           </IonCol>
           <IonCol>
-          <IonButton fill="outline" id='open-modal' color='success' className='reservation_top_button'><IonIcon icon={addOutline} />Ajouter</IonButton>
+            <IonButton fill="outline" id='open-modal' color='success' className='reservation_top_button'><IonIcon icon={addOutline} />Ajouter</IonButton>
           </IonCol>
         </IonRow>
 
